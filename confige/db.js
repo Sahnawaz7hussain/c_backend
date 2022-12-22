a69f2d30a2f1dadb7101ef7cbfa891a7f0b4f7a9
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-12-22 14:24:53 +0530

    clusr0added

--- confige/db.js
+++ confige/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/chatdbs");
+//const connection = mongoose.connect("mongodb://127.0.0.1:27017/chatdbs");
+const connection = mongoose.connect(
+  "mongodb+srv://s:a@cluster0.p6axcnb.mongodb.net/chatdbs?retryWrites=true&w=majority"
+);
 
 module.exports = { connection };
